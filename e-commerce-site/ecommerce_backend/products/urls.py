e8c5a52b25eb14d784a89f40ec2a673b59780a38
Zentimpy . <zuhaib@my.yorku.ca>
2024-11-01 06:13:40 -0400

    created models for customer and Product

--- e-commerce-site/ecommerce_backend/products/urls.py
+++ e-commerce-site/ecommerce_backend/products/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from products import views
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('products.urls')),
+    path('', views.home, name='home'),
 ]
--- e-commerce-site/ecommerce_backend/products/views.py
+++ e-commerce-site/ecommerce_backend/products/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+
+#super user: Username: admin, Password: password
+
+def home(request):
+    return render(request, 'home.html', {})
