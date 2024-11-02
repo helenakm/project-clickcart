f46a51ce07027cb1b8083629c7a80450136c3e9c
Zentimpy . <zuhaib@my.yorku.ca>
2024-11-02 13:15:38 -0400

    login + register update

--- e-commerce-site/ecommerce_backend/products/urls.py
+++ e-commerce-site/ecommerce_backend/products/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_user, name='login'),
