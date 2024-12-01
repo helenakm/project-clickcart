cd490349d42b8e0495391212ed9331f61513624c
Karim He <karimhe1c@gmail.com>
2024-12-01 18:58:02 -0500

    add cart feature added

--- ecommerce_site/ecommerce_backend/products/models.py
+++ ecommerce_site/ecommerce_backend/products/models.py
@@ -6,7 +6,7 @@ class Products(models.Model):
     name = models.CharField(max_length=60)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     description = models.CharField(max_length=1500, blank=True, null=True)
-    image = models.ImageField(upload_to='products', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -30,3 +30,36 @@ class Review(models.Model):
 
     def __str__(self):
         return f'Review by {self.user} for {self.product}'
+    
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True
+    )
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Cart {self.id} for {self.user.username if self.user else 'Guest'}"
+   
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        'Cart',
+        on_delete = models.CASCADE,
+        related_name='items'
+    )
+
+    product = models.ForeignKey(
+        'Products',
+        on_delete = models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(default = 1)
+
+    def get_subtotal(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
--- ecommerce_site/ecommerce_backend/products/urls.py
+++ ecommerce_site/ecommerce_backend/products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import product_list
+from .views import product_list ,ProductViewSet
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from . import views
+from .models import Cart
 
 router = DefaultRouter()
-
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
@@ -19,4 +20,9 @@ urlpatterns = [
     path('reviews/<int:product_id>/', views.get_reviews, name='get_reviews'),
     path('reviews/add/', views.add_review, name='add_review'),
     path('search_bar/', views.search_bar, name='search_bar'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<int:item_id>/', views.update_cart_item, name='update_cart_item'),
+    path('cart/remove/<int:item_id>/', views.remove_cart_item, name='remove_cart_item'),
+
 ]
