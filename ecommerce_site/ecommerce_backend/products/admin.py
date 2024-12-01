8f92ca61eb2d8c1e142643a7f6c4eb6a9d143775
Nargis Rafie <Nargis.rafie00@gmail.com>
2024-12-01 17:56:37 -0500

    Categories added

--- ecommerce_site/ecommerce_backend/products/admin.py
+++ ecommerce_site/ecommerce_backend/products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Products
-from .models import Review
+from .models import *
 
 admin.site.register(Products)
 admin.site.register(Review)
+admin.site.register(Category)
+admin.site.register(Customer)
