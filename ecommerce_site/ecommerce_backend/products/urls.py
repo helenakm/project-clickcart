771805812b7ffa78b6f7441f8c97a6c1fc5275a8
Zentimpy . <zuhaib@my.yorku.ca>
2024-12-01 21:10:01 -0500

    updated cart

--- ecommerce_site/ecommerce_backend/products/urls.py
+++ ecommerce_site/ecommerce_backend/products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import product_list
+from .views import product_list ,ProductViewSet
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from . import views
+from .models import Cart
 
 router = DefaultRouter()
+router.register(r'products', ProductViewSet)
 
 
 urlpatterns = [
@@ -20,4 +22,8 @@ urlpatterns = [
     path('reviews/add/', views.add_review, name='add_review'),
     path('search_bar/', views.search_bar, name='search_bar'),
     path('category/<int:category_id>/', views.category_detail, name='category_detail'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<int:item_id>/', views.update_cart_item, name='update_cart_item'),
+    path('cart/remove/<int:item_id>/', views.remove_cart_item, name='remove_cart_item'),
 ]
